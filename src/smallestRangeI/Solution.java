a1db5baf516c32af3a42ba3b0d86bfdc3bb9eb04
FrozenlemonTee <1115306170@qq.com>
2024-10-20 20:52:34 +0800

    init repo

---
+++ src/smallestRangeI/Solution.java
@@ -0,0 +1,17 @@
+package smallestRangeI;
+
+import java.util.Arrays;
+
+class Solution {
+    public int smallestRangeI(int[] nums, int k) {
+        if(nums.length == 1){
+            return 0;
+        }
+        Arrays.sort(nums);
+        return Math.max((nums[nums.length - 1] - nums[0] - 2 * k), 0);
+    }
+    public int smallestRangeI2(int[] nums, int k) {
+        return Math.max((Arrays.stream(nums).max().orElse(0)
+                - Arrays.stream(nums).min().orElse(0) - 2 * k), 0);
+    }
+}
