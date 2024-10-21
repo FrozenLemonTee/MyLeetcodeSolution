ea66d30bc4e20d7cf48307f3400f2b82534fd645
FrozenlemonTee <1115306170@qq.com>
2024-10-21 15:59:38 +0800

    day2: total-1 accept-1

---
+++ src/twoSum/test.java
@@ -0,0 +1,17 @@
+package twoSum;
+
+import java.util.Arrays;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution().twoSum2(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(Arrays.toString(new Solution().twoSum2(new int[]{3, 2, 4}, 6)));
+        System.out.println(Arrays.toString(new Solution().twoSum2(new int[]{3, 3}, 6)));
+        System.out.println(Arrays.toString(new Solution().twoSum2(new int[]{1, 5, 2, 40, 9, 3}, 41)));
+        System.out.println();
+        System.out.println(Arrays.toString(new Solution().twoSum(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(Arrays.toString(new Solution().twoSum(new int[]{3, 2, 4}, 6)));
+        System.out.println(Arrays.toString(new Solution().twoSum(new int[]{3, 3}, 6)));
+        System.out.println(Arrays.toString(new Solution().twoSum(new int[]{1, 5, 2, 40, 9, 3}, 41)));
+    }
+}
