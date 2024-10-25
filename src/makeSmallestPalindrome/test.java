a8013e7f9706c3639efd5d9013967f6078f5839f
FrozenlemonTee <1115306170@qq.com>
2024-10-25 20:51:29 +0800

    day5: total-8 accept-8

---
+++ src/makeSmallestPalindrome/test.java
@@ -0,0 +1,13 @@
+package makeSmallestPalindrome;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(new Solution().makeSmallestPalindrome("egcfe"));
+        System.out.println(new Solution().makeSmallestPalindrome("abcd"));
+        System.out.println(new Solution().makeSmallestPalindrome("seven"));
+        System.out.println();
+        System.out.println(new Solution().makeSmallestPalindrome2("egcfe"));
+        System.out.println(new Solution().makeSmallestPalindrome2("abcd"));
+        System.out.println(new Solution().makeSmallestPalindrome2("seven"));
+    }
+}
