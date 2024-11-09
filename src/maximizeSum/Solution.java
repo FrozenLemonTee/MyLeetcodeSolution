d9a06eb9cdab5ca4437a1e71826588fa441a14a6
FrozenlemonTee <1115306170@qq.com>
2024-11-09 23:59:55 +0800

    accept-2

---
+++ src/maximizeSum/Solution.java
@@ -0,0 +1,12 @@
+package maximizeSum;
+
+
+public class Solution {
+    public int maximizeSum(int[] nums, int k) {
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+        return (2 * max + k - 1) *k / 2;
+    }
+}
---
+++ src/numJewelsInStones/Solution.java
@@ -0,0 +1,27 @@
+package numJewelsInStones;
+
+public class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int[][]  map = new int[26][2];
+        for (Character c : jewels.toCharArray()) {
+            if (Character.isUpperCase(c)){
+                map[c - 'A'][0]++;
+            }else {
+                map[c - 'a'][1]++;
+            }
+        }
+        int count = 0;
+        for (Character c : stones.toCharArray()) {
+            if (Character.isUpperCase(c)){
+                if (map[c - 'A'][0] != 0){
+                    count++;
+                }
+            }else {
+                if (map[c - 'a'][1] != 0){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
