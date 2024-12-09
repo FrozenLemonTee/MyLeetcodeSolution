0ce1c93be6575d66e7caa5be5c60ff91ad1a33c5
FrozenlemonTee <frozenlemontee@gmail.com>
2024-12-09 10:06:03 +0800

    accept-4

---
+++ src/defangIPaddr/Solution.java
@@ -0,0 +1,17 @@
+package defangIPaddr;
+
+public class Solution {
+    public String defangIPaddr(String address) {
+        return address.replaceAll("[.]", "[.]");
+    }
+
+    public String defangIPaddr2(String address) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : address.toCharArray()) {
+            if (c == '.') {
+                sb.append("[.]");
+            }else sb.append(c);
+        }
+        return sb.toString();
+    }
+}
