4c9484e7c44d17c39989de05175e139805ae0828
FrozenlemonTee <frozenlemontee@gmail.com>
2024-12-11 10:18:38 +0800

    accept-5

---
+++ src/minCount/Solution.java
@@ -0,0 +1,11 @@
+package minCount;
+
+public class Solution {
+    public int minCount(int[] coins) {
+        int count = 0;
+        for (int coin : coins) {
+            count += coin / 2 + coin % 2;
+        }
+        return count;
+    }
+}
