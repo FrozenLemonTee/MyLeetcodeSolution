e557013a79dabe9e6077a0c5cfb1792a09e5cb46
FrozenlemonTee <frozenlemontee@gmail.com>
2024-12-13 10:13:53 +0800

    accept-5

---
+++ src/isAcronym/test.java
@@ -0,0 +1,10 @@
+package isAcronym;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(new Solution().isAcronym(new ArrayList<>(Arrays.asList("alice","bob","charlie")), "abc"));
+    }
+}
