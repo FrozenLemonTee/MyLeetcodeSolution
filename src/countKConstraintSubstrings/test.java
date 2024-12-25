78031bd104f719e52625f75c31f0a527a9cfe4d6
FrozenlemonTee <frozenlemontee@gmail.com>
2024-12-25 11:16:06 +0800

    accept-2

---
+++ src/countKConstraintSubstrings/test.java
@@ -0,0 +1,9 @@
+package countKConstraintSubstrings;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(new Solution().countKConstraintSubstrings("10101", 1));
+        System.out.println(new Solution().countKConstraintSubstrings("1010101", 2));
+        System.out.println(new Solution().countKConstraintSubstrings("11111", 1));
+    }
+}
